1cfa223e47d93d243deec45eb46acade2f263c63
Ronald <ronald.cortezlouy@gmail.com>
2025-01-03 16:09:04 -0300

    archives organization

---
+++ go-essentials/compiling-go/main.go
@@ -0,0 +1,8 @@
+package main
+
+import (
+	"github.com/AndRonald/gotestes/tree/master/go-essentials/compiling-go/printer"
+)
+
+func main(){
+	printer.PrinterSomenthing()}
--- go-essentials/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import (
-	"github.com/AndRonald/gotestes/tree/master/go-essentials/compiling-go/compiling-go/printer"
-)
-
-func main(){
-	printer.PrinterSomenthing()}
